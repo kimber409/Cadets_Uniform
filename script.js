eeed73722eb9dbc71d640a98db038d45077c83ca
kimber409 <oliver@k3r.me>
2023-02-19 00:18:00 +0000

    <commit_message>

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const express = require('express');
 const mysql = require('mysql');
+
+const app = express();
 
 // Create a connection to the MySQL database
 const connection = mysql.createConnection({
@@ -18,42 +21,20 @@ connection.connect((error) => {
     console.log('Connected to the database.');
 });
 
-// Define the new uniform item
-const name = 'New Uniform Item';
-const size = 'L';
-const checked_out = false;
-
-// Insert the new uniform item into the database
-connection.query('INSERT INTO uniform (name, size, checked_out) VALUES (?, ?, ?)', [name, size, checked_out], (error, results, fields) => {
-    if (error) {
-        console.error('Error inserting uniform item: ' + error.stack);
-        return;
-    }
-
-    console.log('Uniform item inserted successfully.');
-
-    // Retrieve the list of uniform items from the database
+// Define the API endpoint that returns the list of uniform items in JSON format
+app.get('/api/uniform', (req, res) => {
     connection.query('SELECT * FROM uniform', (error, results, fields) => {
         if (error) {
             console.error('Error retrieving uniform items: ' + error.stack);
-            return;
+            return res.status(500).json({ error: 'Error retrieving uniform items' });
         }
 
-        // Generate the HTML for the list of uniform items
-        let html = '';
-        results.forEach((item) => {
-            html += '<div>' +
-                '<h2>' + item.name + '</h2>' +
-                '<p>Size: ' + item.size + '</p>' +
-                '<p>Checked Out: ' + (item.checked_out ? 'Yes' : 'No') + '</p>' +
-                '</div>';
-        });
-
-        // Insert the HTML into the page
-        const uniformList = document.getElementById('uniform-list');
-        uniformList.innerHTML = html;
-
-        // Close the database connection
-        connection.end();
+        res.json(results);
     });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}.`);
 });
